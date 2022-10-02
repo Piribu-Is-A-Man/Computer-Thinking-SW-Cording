4c15fd95173fe97037b67805f629c0447c67ab33
Piribu-Is-A-Man <114078946+Piribu-Is-A-Man@users.noreply.github.com>
2022-10-02 22:57:07 +0900

    Update Q5.py

--- 1주차/EXERCISE/Q5.py
+++ 1주차/EXERCISE/Q5.py
@@ -1,11 +1,31 @@
-#221002
-#4주차까지의 개념을 배운 후 코딩
+#1주차 내용으로 만든 코드
 
-#factorial
+#print('3!=',3*2*1)
+#print('5!=',5*4*3*2*1)
+#print('12!=',12*11*10*9*8*7*6*5*4*3*2*1)
+#print('20!=',20*19*18*17*16*15*14*13*12*11*10*9*8*7*6*5*4*3*2*1)
 
-num=int(input())
+#4주차 내용으로 만든 코드
+num=3
 fac=1
 for i in range(0,num):
     fac=fac*(i+1)
+print(num,'! = ',fac)
 
+num=5
+fac=1
+for i in range(0,num):
+    fac=fac*(i+1)
+print(num,'! = ',fac)
+
+num=12
+fac=1
+for i in range(0,num):
+    fac=fac*(i+1)
+print(num,'! = ',fac)
+
+num=20
+fac=1
+for i in range(0,num):
+    fac=fac*(i+1)
 print(num,'! = ',fac)
