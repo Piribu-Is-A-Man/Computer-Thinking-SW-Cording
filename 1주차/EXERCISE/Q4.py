4874bc931b5ebab5b155d1eef450170b4fb54af2
Piribu-Is-A-Man <114078946+Piribu-Is-A-Man@users.noreply.github.com>
2022-10-02 22:57:57 +0900

    Update Q4.py

--- 1주차/EXERCISE/Q4.py
+++ 1주차/EXERCISE/Q4.py
@@ -1,5 +1,11 @@
 #triangle.py
+
+#1주차 내용으로 만든 코드
 print('   *   ')
 print('  ***  ')
 print(' ***** ')
 print('*******')
+
+#4주차 내용으로 만든 코드
+for i in range(4):
+    print(' '*(3-i)+'*'*(2*i+1)+' '*(3-i))
